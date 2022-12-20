2602be65bf4a964ac4e3a826091099b2328a2c49
Paul Shorey <web@artspaces.net>
2022-12-19 19:53:24 -0600

    removed .turbo from each project

--- _bash/commit-all.sh
+++ _bash/commit-all.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 # Optionally, make this file executable: chmod +x recursive_iteration.sh
 # Or call it with: bash recursive_iteration.sh
+cwd=$( cd "$(dirname "$0")" ; pwd -P )
+echo $cwd
+rootdir=$cwd/../
 
 # Function to be called for each directory
 function process_dir {
+  # Make sure the input is a relative path
+  if [[ $1 == /* ]]; then
+    echo "Error: Input must be a relative path."
+    return 1
+  fi
   # $1 is the directory passed as an argument to the function
   if [ -d "$1/.git" ]; then
-    echo "Committing any changes in $1" &&
+    echo -e "\033[0;33mCommitting any changes in $1 \033[0m" &&
+    cd "$rootdir$1" 
+    echo $(pwd) &&
+    cd $rootdir &&
     git add . && 
     git pull && 
+    git add . &&
     git commit -m "$2" && 
     git push
   fi
@@ -17,9 +29,10 @@ function process_dir {
 # Input directory
 input_dir=$1
 commit_message=$2
+echo -e "\033[0;31minput_dir= $input_dir\033[0m"
+echo -e "\033[0;31mcommit_message= $commit_message\033[0m"
 
 # Recursively iterate through every subdirectory in the input directory
 for dir in $(find "$input_dir" -maxdepth 2 -type d ! -path "*node_modules*" ! -path "*node_modules_x*"); do
-  # Call the function for each directory
   process_dir "$dir" "$commit_message"
 done
