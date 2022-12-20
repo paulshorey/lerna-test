47fd7465b62e4755becf09f1f970b8ea0ea67214
Paul Shorey <web@artspaces.net>
2022-12-19 19:23:13 -0600

    removed .turbo from each project

---
+++ _bash/commit-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Optionally, make this file executable: chmod +x recursive_iteration.sh
+# Or call it with: bash recursive_iteration.sh
+
+# Function to be called for each directory
+function process_dir {
+  # $1 is the directory passed as an argument to the function
+  if [ -d "$1/.git" ]; then
+    echo "Committing any changes in $1"
+    git fetch --all
+    git add .
+    git pull 
+    git commit -m "$2"
+    git push
+  fi
+}
+
+# Input directory
+input_dir=$1
+commit_message=$2
+
+# Recursively iterate through every subdirectory in the input directory
+for dir in $(find "$input_dir" -maxdepth 2 -type d ! -path "*node_modules*" ! -path "*node_modules_x*"); do
+  # Call the function for each directory
+  process_dir "$dir" "$commit_message"
+done
