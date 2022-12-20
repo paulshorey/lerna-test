2fc34b403c6256a5377c359c25b17eaffc92bc1d
Paul Shorey <web@artspaces.net>
2022-12-20 16:11:02 -0600

    test commit bash script

--- _bin/commit-all.sh
+++ _bin/commit-all.sh
@@ -3,11 +3,29 @@
 # Usage: bash _bin/commit-all.sh <folder-to-start-at> <commit-message>
 # bash _bin/commit-all.sh . 'fixed npm dependencies so all modules work in mono/multi repo'
 
+if [[ ! $1 ]]; then
+  echo -e "\n" "Error: Must specify starting directory (as first parameter)" "\n"
+  exit 1
+fi
+if [[ ! $2 ]]; then
+  echo -e "\n" "Error: Must specify git commit message (as second parameter)" "\n"
+  exit 1
+fi
+
 # Optionally, make this file executable: chmod +x recursive_iteration.sh
 # Or call it with: bash recursive_iteration.sh
 cwd=$( cd "$(dirname "$0")" ; pwd -P )
 echo $cwd
 rootdir=$cwd/../
+
+# Save changes to git
+function git_save {
+  git add .
+  git pull
+  git add .
+  git commit -m "$1"
+  git push
+}
 
 # Function to be called for each directory
 function process_dir {
@@ -22,23 +40,22 @@ function process_dir {
     if [ -d "$1/.git" ]; then
       echo -e "\n\033[0;33mAttempting to commit changes in \033[0m" "\033[0;32m$1\033[0m"
       cd "$rootdir$1"
-      git add .
-      git pull
-      git add .
-      git commit -m "$2"
-      git push
+      git_save "$2"
       cd $rootdir
     fi
   fi
 }
 
 # Input directory
-input_dir=$1
-commit_message=$2
+# input_dir=$1
+# commit_message=$2
 # echo -e "\033[0;31minput_dir= $input_dir\033[0m"
 # echo -e "\033[0;31mcommit_message= $commit_message\033[0m"
 
 # Recursively iterate through every subdirectory in the input directory
-for dir in $(find "$input_dir" -maxdepth 2 -type d ! -path "*node_modules*" ! -path "*node_modules_x*"); do
-  process_dir "$dir" "$commit_message"
+for dir in $(find "$1" -maxdepth 2 -type d ! -path "*node_modules*" ! -path "*node_modules_x*"); do
+  process_dir "$dir" "$2"
 done
+
+# Commit the root
+git_save "$2"
